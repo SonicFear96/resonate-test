01df25532d20a5da99a8f11988cf9609b7789363
Bogdan Rudakov <48532613+SonicFear96@users.noreply.github.com>
2022-01-26 23:01:37 +0300

    fix: gulp

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ const gulp = require("gulp");
 const ghPages = require("gulp-gh-pages");
 
 gulp.task("deploy", function () {
-  return gulp.src("./dest/**/*").pipe(ghPages());
+  return gulp.src("./dist/**/*").pipe(ghPages());
 });
 
 function html() {
